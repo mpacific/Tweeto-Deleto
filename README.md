b8ada7ac22d498f93956cd48cc439400dd175c18
Mike Pacific <mike.pacific@gmail.com>
2018-03-10 01:52:31 -0800

    Tweet deleting.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Node Delete Your Tweets
 A script to run at a scheduled interval to delete your tweets and likes at a certain threshold. Work in progress.
 
-## TODO 
-- Targeting tweets at a threshold
-- Deleting old tweets
+## TODO
+- Fetching likes
+- Deleting old likes
 - Tests
 - Fleshing out this README
--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 require('dotenv').config()
-const twitter = require('./lib/twitter.js')
+const Twitter = require('./lib/twitter')
+const _ = require('lodash')
+const Moment = require('moment')
 
-twitter.get_timeline().then((timeline) => {
-  console.log('Timeline', timeline.length)
+Twitter.get_timeline().then((tweets) => {
+  if (tweets.length === 0) {
+    console.warn('No tweets found!')
+    return
+  }
+
+  console.log(`Totla tweets: ${tweets.length}`)
+
+  let oldTweets = []
+  _.forEach(tweets, (tweet) => {
+    if (new Moment().diff(new Moment(new Date(tweet.created_at)), 'days') >= process.env.TWITTER_MAX_DAYS) {
+      oldTweets.push(tweet)
+    }
+  })
+
+  console.log(`Old tweets: ${oldTweets.length}`)
+  if (oldTweets.length > 0) {
+    _.forEach(oldTweets, (tweet) => {
+      Twitter.request(`statuses/destroy/${tweet.id_str}`, 'POST', {
+        id: tweet.id_str
+      }).then((deletedTweet) => {
+        console.log(`Deleted tweet: ${tweet.text} (ID ${tweet.id_str})`)
+      }).catch((error) => {
+        console.log(`Could not delete tweet: ${tweet.text} (ID ${tweet.id_str}) - Reason: ${error}`)
+      })
+    })
+  }
 }).catch((error) => {
   console.error(`Error: ${error}`)
 })
