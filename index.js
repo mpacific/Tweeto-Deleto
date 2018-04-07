0956466efa16a82e2ecef6f4bb44eba10d547736
Mike Pacific <mike.pacific@gmail.com>
2018-04-06 21:18:22 -0700

    Add automatic unliking

--- index.js
+++ index.js
@@ -3,35 +3,72 @@ const Twitter = require('./lib/twitter')
 const _ = require('lodash')
 const Moment = require('moment')
 
-console.log(`Started tweet deleting at: ${new Moment().format()}`)
+if (process.env.TWITTER_DELETE_TWEETS === 'true') {
+  console.log(`Started tweet deleting at: ${new Moment().format()}`)
 
-Twitter.get_timeline().then((tweets) => {
-  if (tweets.length === 0) {
-    console.warn('No tweets found!')
-    return
-  }
-
-  console.log(`Total tweets: ${tweets.length}`)
-
-  let oldTweets = []
-  _.forEach(tweets, (tweet) => {
-    if (new Moment().diff(new Moment(new Date(tweet.created_at)), 'days') >= process.env.TWITTER_MAX_DAYS) {
-      oldTweets.push(tweet)
+  Twitter.get_timeline().then((tweets) => {
+    if (tweets.length === 0) {
+      console.warn('No tweets found!')
+      return
     }
-  })
 
-  console.log(`Old tweets: ${oldTweets.length}`)
-  if (oldTweets.length > 0) {
-    _.forEach(oldTweets, (tweet) => {
-      Twitter.request(`statuses/destroy/${tweet.id_str}`, 'POST', {
-        id: tweet.id_str
-      }).then((deletedTweet) => {
-        console.log(`Deleted tweet: ${tweet.text} (ID ${tweet.id_str})`)
-      }).catch((error) => {
-        console.log(`Could not delete tweet: ${tweet.text} (ID ${tweet.id_str}) - Reason: ${error}`)
-      })
+    console.log(`Total tweets: ${tweets.length}`)
+
+    let oldTweets = []
+    _.forEach(tweets, (tweet) => {
+      if (new Moment().diff(new Moment(new Date(tweet.created_at)), 'days') >= process.env.TWITTER_MAX_DAYS) {
+        oldTweets.push(tweet)
+      }
     })
-  }
-}).catch((error) => {
-  console.error(`Error: ${error}`)
-})
+
+    console.log(`Old tweets: ${oldTweets.length}`)
+    if (oldTweets.length > 0) {
+      _.forEach(oldTweets, (tweet) => {
+        Twitter.request(`statuses/destroy/${tweet.id_str}`, 'POST', {
+          id: tweet.id_str
+        }).then((deletedTweet) => {
+          console.log(`Deleted tweet: ${tweet.text} (ID ${tweet.id_str})`)
+        }).catch((error) => {
+          console.log(`Could not delete tweet: ${tweet.text} (ID ${tweet.id_str}) - Reason: ${error}`)
+        })
+      })
+    }
+  }).catch((error) => {
+    console.error(`Tweet error: ${error}`)
+  })
+}
+
+if (process.env.TWITTER_DELETE_LIKES === 'true') {
+  console.log(`Started unliking at: ${new Moment().format()}`)
+
+  Twitter.get_likes().then((likes) => {
+    if (likes.length === 0) {
+      console.warn('No likes found!')
+      return
+    }
+
+    console.log(`Total likes: ${likes.length}`)
+
+    let oldLikes = []
+    _.forEach(likes, (like) => {
+      if (new Moment().diff(new Moment(new Date(like.created_at)), 'days') >= process.env.TWITTER_MAX_DAYS) {
+        oldLikes.push(like)
+      }
+    })
+
+    console.log(`Old likes: ${oldLikes.length}`)
+    if (oldLikes.length > 0) {
+      _.forEach(oldLikes, (like) => {
+        Twitter.request(`favorites/destroy`, 'POST', {
+          id: like.id_str
+        }).then((deletedLike) => {
+          console.log(`Unliked: ${like.text} (ID ${like.id_str})`)
+        }).catch((error) => {
+          console.log(`Could not unlike: ${like.text} (ID ${like.id_str}) - Reason: ${error}`)
+        })
+      })
+    }
+  }).catch((error) => {
+    console.error(`Like error: ${error}`)
+  })
+}
